446f3745f6b6c60cbe72572f26aef272dc1f4a2d
macie <macie@DESKTOP-GUK2RJ2>
2021-04-14 14:08:36 +0200

    Add project files.

---
+++ SingleVsFirst/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SingleVsFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
